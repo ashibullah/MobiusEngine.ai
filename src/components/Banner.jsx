ddb2157399a1f5e5ffec4249702523f4ffe2c4a3
ashibullah <ashibullahbhai@gmail.com>
2025-04-26 23:33:33 +0600

    banner done

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,22 +1,49 @@
 import React from 'react';
 import bannerImg from '../assets/banner.png';
 import bgImg from '../assets/bannerBg.png';
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
+import pdfImg from '../assets/image 1.png';
+import bookImg from '../assets/book.png';
+import { GoArrowDownLeft, GoArrowRight } from 'react-icons/go';
 
 const Banner = () => {
     return (
-        <div className='flex items-center justify-center py-20' style={{ backgroundImage: `url(${bgImg})` }}>
+        <div className='flex items-start justify-center pt-40 pb-20 text-white'
+            style={{
+                backgroundImage: `url(${bgImg})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                backgroundRepeat: 'no-repeat'
+            }}>
             <div className='flex flex-col gap-8 w-[600px] items-start'>
                 <h1 className='text-Sora text-5xl font-bold '>Land job interviews <br /> <span className='text-darkblue'>10x</span> faster</h1>
-                <p>Custom-built resumes that match your goals, keywords, and recruiter expectations.</p>
-                <button className='text-[20px] bg-white text-black px-6 py-2 rounded-[32px]'>Get Started</button>
+                <p className='font-light'>Custom-built resumes that match your goals, keywords, and recruiter expectations.</p>
+                <div className='text-lg bg-white text-black px-6 py-3 rounded-full flex items-center gap-2  text-darkblue'>
+                    <button >Get Started</button>
+                    <GoArrowRight />
+
+                </div>
 
 
             </div>
             <div>
-                <img className='w-full' src={bannerImg} alt="banner" />
-
+            <div className='relative'>
+                <img className='w-xs ' src={pdfImg} alt="banner" />
+                <div className='backdrop-blur-xs p-5 rounded-full border-2 absolute -bottom-7 -right-6'>
+                    <img  src={bookImg} alt="banner" />
+                </div>
+                <div className='flex justify-center items-center w-6 h-6 text-darkblue bg-white rounded-full 
+                absolute -bottom-9 right-10'>
+                   <GoArrowDownLeft />
+                </div>               
+            </div>
+            <div className='pt-5'>
+             <p className='font-light text-center '>Download Free E-Book</p>
+   
             </div>
             
+
+        </div>
         </div>
     );
 };
