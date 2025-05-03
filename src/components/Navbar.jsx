9abae0d58c8740b79dec56d30e1853ebb4402867
ashibullah <ashibullahbhai@gmail.com>
2025-05-04 01:37:28 +0600

    responsive banner

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,55 @@
-import React from 'react';
-import { IoMdArrowDropdown } from 'react-icons/io';
+import React, { useState } from 'react';
+import { IoMdArrowDropdown, IoMdMenu, IoMdClose } from 'react-icons/io';
 import logo from '../assets/Startlogo.png';
 
-
 const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
-        <div className='flex justify-between items-center px-[85px] py-5 text-white fixed top-0 left-0 w-full'>
-            <div>
-                <img className='w-40' src={logo} alt="logo" />
-            </div>
-            <div>
-                <ul className='flex gap-5'>
+        <div className='bg-transparent fixed top-0 left-0 w-full z-50 text-white'>
+            <div className='flex justify-between items-center px-5 md:px-[85px] py-4 md:py-5'>
+                <div>
+                    <img className='w-32 md:w-40' src={logo} alt="logo" />
+                </div>
+
+                <div className='block lg:hidden'>
+                    <button onClick={() => setMenuOpen(!menuOpen)}>
+                        {menuOpen ? <IoMdClose size={28} /> : <IoMdMenu size={28} />}
+                    </button>
+                </div>
+
+                <ul className='hidden lg:flex gap-5 items-center'>
                     <li>Home</li>
                     <li>About us</li>
                     <li>Plans</li>
                     <li>Testimonials</li>
                     <li>Privacy Policy</li>
-                    <li>Testimonials</li>
-                    <li className='flex items-center'>More <IoMdArrowDropdown /></li>
+                    <li className='flex items-center gap-1'>More <IoMdArrowDropdown /></li>
+                    <li>
+                        <button className='text-sm md:text-[16px] bg-white font-semibold text-black px-5 md:px-6 py-2 rounded-full'>
+                            Get Started
+                        </button>
+                    </li>
                 </ul>
             </div>
-            <div>
-                <button className='text-[16px] bg-white font-semibold text-black px-6 py-2 rounded-[32px] text-darkblue'>Get Started</button>
-            </div>
+
+            {menuOpen && (
+                <div className='lg:hidden bg-[#111827] text-white px-5 pb-4'>
+                    <ul className='flex flex-col gap-3'>
+                        <li>Home</li>
+                        <li>About us</li>
+                        <li>Plans</li>
+                        <li>Testimonials</li>
+                        <li>Privacy Policy</li>
+                        <li className='flex items-center gap-1'>More <IoMdArrowDropdown /></li>
+                        <li>
+                            <button className='w-full text-sm bg-white font-semibold text-black px-4 py-2 rounded-full'>
+                                Get Started
+                            </button>
+                        </li>
+                    </ul>
+                </div>
+            )}
         </div>
     );
 };
