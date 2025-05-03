59382c4d26bad4820d4cf5a3562a3e7018f5ab46
ashibullah <ashibullahbhai@gmail.com>
2025-05-04 02:01:23 +0600

    responsive

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -3,15 +3,25 @@ import { GoArrowRight } from 'react-icons/go';
 
 const ContactUs = () => {
     return (
-        <div className=" px-10 py-18 w-[1300px] mx-auto">
-        <div className='text-white font-[600] p-10 py-30  w-full -mt-15 bg-[#0649E7] rounded-4xl grid grid-cols-8 justify-items-center items-center gap-35 py-20'>
-                        <p className='col-span-2 uppercase text-[28px] font-[400] self-start'>Still Have Doubts?</p>
-                        <h2 className='text-7xl font-[500] col-span-4'>Contact us</h2>
-                        <GoArrowRight className='col-span-2 text-[80px] font-semibold bg-white text-[#0649E7] rounded-full p-4 self-end'/>
-                       
-        
-                    </div>
-                    </div>
+        <div className="px-4 md:px-10 py-18 mx-auto">
+            <div className="text-white font-semibold p-10 w-full bg-[#0649E7] rounded-4xl grid grid-cols-1 md:grid-cols-8 gap-8 md:gap-16 py-20">
+                
+          
+                <p className="hidden md:block col-span-1 md:col-span-2 uppercase text-2xl md:text-[28px] font-light self-start">
+                    Still Have Doubts?
+                </p>
+                
+           
+                <h2 className="text-4xl md:text-7xl font-medium col-span-1 md:col-span-4 text-center md:text-left">
+                    Contact us
+                </h2>
+
+
+                <div className="col-span-1 md:col-span-2 flex justify-center md:justify-end">
+                    <GoArrowRight className="text-6xl md:text-[80px] font-semibold bg-white text-[#0649E7] rounded-full p-4" />
+                </div>
+            </div>
+        </div>
     );
 };
 
